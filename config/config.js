cb0e74c680f514f5e0e52c1e39c299f32cc6b7b0
Justin Rice <jsrice7391@gmail.com>
2020-01-12 22:49:31 -0500

    Models and some seq

--- config/config.js
+++ config/config.js
@@ -5,14 +5,12 @@ module.exports = {
     "username": "root",
     "password": process.env.DB_PASS,
     "database": "database_development",
-    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
-    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
